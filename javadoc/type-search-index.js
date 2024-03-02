0f5a303cea245ebdd90f0875361a1387690ca454
shons <shon.slobodkin@gmail.com>
2024-03-02 12:40:44 +0200

    Commit Changes

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BoardModel"},{"p":"<Unnamed>","l":"Presenter.BoardPaint"},{"p":"<Unnamed>","l":"IView"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Presenter"},{"p":"<Unnamed>","l":"View"}];updateSearchResults();
